5c5e9fb4e9fefc2b5dfa2aca6d0093b7d254b801
jungjisu-999 <jisu1008@inha.edu>
2025-08-06 20:12:34 +0900

    refactor: StartPage.jsx 삭제 및 페이지 구조 개선

--- frontend-vite/src/App.jsx
+++ frontend-vite/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Splash from './pages/Splash';
-import StartPage from './pages/StartPage';
 import HomeScreen from './pages/HomeScreen';
 import CameraScreen from './pages/CameraScreen'; 
 import LoadingPage from './pages/LoadingPage';
@@ -13,7 +12,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Splash />} />
-        <Route path="/start" element={<StartPage />} />
         <Route path="/home" element={<HomeScreen />} />
         <Route path="/camera" element={<CameraScreen />} />
         <Route path="/load" element={<LoadingPage />} />
