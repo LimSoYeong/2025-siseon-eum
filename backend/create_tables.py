93db835614b28f6953b8ffa33c2171bcbf55c5d9
LimSoYeong <soyeong020420@gmail.com>
2025-08-06 15:00:10 +0900

    chore: 레거시 코드 삭제

--- backend/create_tables.py
+++
@@ -1,4 +0,0 @@
-from db import Base, engine
-from models import Summary
-
-Base.metadata.create_all(bind=engine)
--- backend/services/gpt_service.py
+++
@@ -1,3 +0,0 @@
-def chat_with_context(user_text: str) -> str:
-    # TODO: 
-    return f"'{user_text}'에 대한 응답입니다."
