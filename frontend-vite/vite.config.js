c713d89818520b8856c8305d9f730058712dd6cf
LimSoYeong <soyeong020420@gmail.com>
2025-08-13 23:59:17 +0900

    fix: favicon 변경

--- frontend-vite/vite.config.js
+++ frontend-vite/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico'],
+      includeAssets: ['favicon.png'],
       manifest: {
         name: '시선이음',
         short_name: '시선이음',
