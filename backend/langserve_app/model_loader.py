c7a97be36662948131f02195762366a11aca71a9
LimSoYeong <soyeong020420@gmail.com>
2025-08-15 13:59:03 +0900

    feat: bfloat16, flash_attention_2 사용 > 추론 속도 향상

--- backend/langserve_app/model_loader.py
+++ backend/langserve_app/model_loader.py
@@ -15,9 +15,11 @@ from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor
 
 @lru_cache()
 def get_model():
+    torch.backends.cuda.matmul.allow_tf32 = True
     model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
         "Qwen/Qwen2.5-VL-7B-Instruct",
-        torch_dtype=torch.float16,
+        torch_dtype=torch.bfloat16,
+        attn_implementation="flash_attention_2",
         device_map={"": "cuda:0"}  # "cuda:0"은 실질적으로 GPU 0 (visible device index 기준)
     )
     return model.eval()
